7774d3e7a2aa6a2ae3d388933adcf97959876bd8
Synapse75 <gzz060316@gmail.com>
2026-03-01 21:57:50 +0800

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "World"
+	fmt.Println(name)
+
+	var name1 = "Go"
+	fmt.Println(name1)
+
+	name2 := "Programming"
+	fmt.Println(name2, "is fun!", name1)
+}
