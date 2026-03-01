a484f41db19bd6acd5921067c7cae0275a07f594
Synapse75 <gzz060316@gmail.com>
2026-03-01 23:44:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,4 +12,16 @@ func main() {
 
 	name2 := "Programming"
 	fmt.Println(name2, "is fun!", name1)
+
+	var a byte = 'a'
+	fmt.Printf("The value of a is: %d\n", a)
+
+	var b bool = true
+	fmt.Printf("The value of b is: %t\n", b)
+
+	var nameList [3]string = [3]string{"Alice", "Bob", "Charlie"}
+	fmt.Println("Name List:", nameList)
+
+	var ageList = make([]int, 3, 5)
+	fmt.Println("Age List:", ageList)
 }
